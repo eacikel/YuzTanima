066a2252f6f74fa70d9ae601d8dac7ec23f2f2f4
Eren Açıkel <43996358+eacikel@users.noreply.github.com>
2020-03-24 14:02:40 +0300

    Realtime eğitim ve çalışma ile Yüz tanıma

---
+++ WindowsFormsApp56/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace WindowsFormsApp56
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEgit = new System.Windows.Forms.Button();
+            this.lblEgitilenAdet = new System.Windows.Forms.Label();
+            this.txtFaceName = new System.Windows.Forms.TextBox();
+            this.btnEgitimSil = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.txtcocuk = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEgit
+            // 
+            this.btnEgit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(200)))), ((int)(((byte)(0)))));
+            this.btnEgit.Location = new System.Drawing.Point(1233, 132);
+            this.btnEgit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEgit.Name = "btnEgit";
+            this.btnEgit.Size = new System.Drawing.Size(135, 65);
+            this.btnEgit.TabIndex = 1;
+            this.btnEgit.Text = "Veri Al (200 Adet)";
+            this.btnEgit.UseVisualStyleBackColor = false;
+            this.btnEgit.Click += new System.EventHandler(this.btnEgit_Click);
+            // 
+            // lblEgitilenAdet
+            // 
+            this.lblEgitilenAdet.AutoSize = true;
+            this.lblEgitilenAdet.Location = new System.Drawing.Point(1236, 210);
+            this.lblEgitilenAdet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEgitilenAdet.Name = "lblEgitilenAdet";
+            this.lblEgitilenAdet.Size = new System.Drawing.Size(116, 17);
+            this.lblEgitilenAdet.TabIndex = 2;
+            this.lblEgitilenAdet.Text = "Alınan Veri Adedi";
+            // 
+            // txtFaceName
+            // 
+            this.txtFaceName.Location = new System.Drawing.Point(1234, 16);
+            this.txtFaceName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFaceName.Name = "txtFaceName";
+            this.txtFaceName.Size = new System.Drawing.Size(135, 22);
+            this.txtFaceName.TabIndex = 4;
+            this.txtFaceName.Text = "Ad Soyad Giriniz...";
+            this.txtFaceName.TextChanged += new System.EventHandler(this.TxtFaceName_TextChanged);
+            // 
+            // btnEgitimSil
+            // 
+            this.btnEgitimSil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(200)))), ((int)(((byte)(0)))));
+            this.btnEgitimSil.Location = new System.Drawing.Point(1236, 384);
+            this.btnEgitimSil.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEgitimSil.Name = "btnEgitimSil";
+            this.btnEgitimSil.Size = new System.Drawing.Size(136, 64);
+            this.btnEgitimSil.TabIndex = 5;
+            this.btnEgitimSil.Text = "Tüm Verileri Sil";
+            this.btnEgitimSil.UseVisualStyleBackColor = false;
+            this.btnEgitimSil.Click += new System.EventHandler(this.btnEgitimSil_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(1233, 44);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(135, 24);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Enabled = false;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(1233, 102);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(135, 24);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2_SelectedIndexChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1236, 230);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(136, 146);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BackgroundImage = global::WindowsFormsApp56.Properties.Resources.gg;
+            this.pictureBox1.Location = new System.Drawing.Point(104, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1024, 660);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApp56.Properties.Resources.c_;
+            this.pictureBox3.Location = new System.Drawing.Point(1235, 507);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(69, 53);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.PictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::WindowsFormsApp56.Properties.Resources.indir;
+            this.pictureBox4.Location = new System.Drawing.Point(1235, 567);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(69, 53);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::WindowsFormsApp56.Properties.Resources.indir__1_;
+            this.pictureBox5.Location = new System.Drawing.Point(1301, 567);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(69, 53);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 10;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.PictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.White;
+            this.pictureBox6.Image = global::WindowsFormsApp56.Properties.Resources.visual_studio_2013_logo;
+            this.pictureBox6.Location = new System.Drawing.Point(1301, 507);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(69, 53);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 11;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.PictureBox6_Click);
+            // 
+            // txtcocuk
+            // 
+            this.txtcocuk.Location = new System.Drawing.Point(1233, 74);
+            this.txtcocuk.Margin = new System.Windows.Forms.Padding(4);
+            this.txtcocuk.Name = "txtcocuk";
+            this.txtcocuk.Size = new System.Drawing.Size(135, 22);
+            this.txtcocuk.TabIndex = 12;
+            this.txtcocuk.Text = "Çocuk...";
+            this.txtcocuk.TextChanged += new System.EventHandler(this.Txtcocuk_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1482, 668);
+            this.Controls.Add(this.txtcocuk);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.btnEgitimSil);
+            this.Controls.Add(this.txtFaceName);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblEgitilenAdet);
+            this.Controls.Add(this.btnEgit);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Güvenli Kreşim Arayüz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnEgit;
+        private System.Windows.Forms.Label lblEgitilenAdet;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txtFaceName;
+        private System.Windows.Forms.Button btnEgitimSil;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.TextBox txtcocuk;
+    }
+}
+
---
+++ WindowsFormsApp56/Form1.cs
@@ -0,0 +1,157 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp56
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        private async void btnEgit_Click(object sender, EventArgs e)
+        {
+            ToolTip tt = new ToolTip();
+            tt.IsBalloon = true;
+            tt.InitialDelay = 0;
+            tt.ShowAlways = true;
+            tt.SetToolTip(btnEgit, "GÖRÜNTÜ ALINIYOR LÜTFEN KAMERAYA BAKIN!");
+
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 200; i++)
+                {
+                    if (!recognition.SaveTrainingData(pictureBox2.Image, txtFaceName.Text, txtcocuk.Text, comboBox1.Text)) MessageBox.Show("Hata", "Profil alınırken beklenmeyen bir hata oluştu.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Thread.Sleep(100);
+                    lblEgitilenAdet.Text = (i + 1) + " adet profil.";
+                }
+
+
+                recognition = null;
+                train = null;
+
+                recognition = new BusinessRecognition("D:\\", "Faces", "yuz.xml");
+                train = new Classifier_Train("D:\\", "Faces", "yuz.xml");
+            });
+
+        }
+        BusinessRecognition recognition = new BusinessRecognition("D:\\", "Faces", "yuz.xml");
+        Classifier_Train train = new Classifier_Train("D:\\", "Faces", "yuz.xml");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Cocuk");
+            comboBox1.Items.Add("Baba");
+            comboBox1.Items.Add("Anne");
+            comboBox1.Items.Add("Kardes");
+            comboBox1.Items.Add("Buyukanne");
+            comboBox1.Items.Add("Buyukbaba");
+            comboBox1.Items.Add("Komsu");
+
+            Capture capture = new Capture();
+            capture.Start();
+            capture.ImageGrabbed += (a, b) =>
+            {
+                var image = capture.RetrieveBgrFrame();
+                var grayimage = image.Convert<Gray, byte>();
+                HaarCascade haaryuz = new HaarCascade("haarcascade_frontalface_alt2.xml");
+                MCvAvgComp[][] Yuzler = grayimage.DetectHaarCascade(haaryuz, 1.2, 5, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(15, 15));
+                MCvFont font = new MCvFont(FONT.CV_FONT_HERSHEY_DUPLEX, 0.8, 0.8);
+                foreach (MCvAvgComp yuz in Yuzler[0])
+                {
+                    var sadeyuz = grayimage.Copy(yuz.rect).Convert<Gray, byte>().Resize(100, 100, INTER.CV_INTER_CUBIC);
+                    //Resimler aynı boyutta olmalıdır. O yüzden Resize ile yeniden boyutlandırma yapılmıştır. Aksi taktirde Classifier_Train sınıfının 245. satırında hata alınacaktır.
+                    pictureBox2.Image = sadeyuz.ToBitmap();
+                    if (train != null)
+                        if (train.IsTrained)
+                        {
+                            string name = train.Recognise(sadeyuz);
+                            int match_value = (int)train.Get_Eigen_Distance;
+                            image.Draw(name + " ", ref font, new Point(yuz.rect.X - 2, yuz.rect.Y - 2), new Bgr(Color.Blue));
+                            image.Draw(yuz.rect, new Bgr(Color.Orange), 3);
+                        }
+                        else
+                        {
+                            string yabanci = "YABANCI";
+                            image.Draw(yabanci + " ", ref font, new Point(yuz.rect.X - 2, yuz.rect.Y - 2), new Bgr(Color.Blue));
+                            image.Draw(yuz.rect, new Bgr(Color.Orange), 3);
+                        }
+                } pictureBox1.Image = image.ToBitmap();
+            };
+        }
+
+        private void btnEgitimSil_Click(object sender, EventArgs e)
+        {
+            recognition.DeleteTrains();
+            ToolTip tt = new ToolTip();
+            tt.IsBalloon = true;
+            tt.InitialDelay = 0;
+            tt.ShowAlways = true;
+            tt.SetToolTip(btnEgitimSil, "TÜM VERİLER SİLİNDİ KONTROL EDİN!");
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Cocuk")
+            {
+                comboBox2.Items.Clear();
+         
+
+            }
+            else
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("çocuk1");
+                comboBox2.Items.Add("çocuk2");
+                comboBox2.Items.Add("çocuk3");
+            }
+        }
+
+        private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtFaceName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Txtcocuk_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
